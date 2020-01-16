9ab177ee0f7cf8036472e48954680f334da90f28
Daria Diachkova <dariadiachkova@mail.ru>
2020-01-16 18:54:12 +0300

    minor refactoring

--- src/js/Orders.js
+++ src/js/Orders.js
@@ -7,54 +7,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Title from "./Title";
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(
-    0,
-    "16 Mar, 2019",
-    "Elvis Presley",
-    "Tupelo, MS",
-    "VISA ⠀•••• 3719",
-    312.44
-  ),
-  createData(
-    1,
-    "16 Mar, 2019",
-    "Paul McCartney",
-    "London, UK",
-    "VISA ⠀•••• 2574",
-    866.99
-  ),
-  createData(
-    2,
-    "16 Mar, 2019",
-    "Tom Scholz",
-    "Boston, MA",
-    "MC ⠀•••• 1253",
-    100.81
-  ),
-  createData(
-    3,
-    "16 Mar, 2019",
-    "Michael Jackson",
-    "Gary, IN",
-    "AMEX ⠀•••• 2000",
-    654.39
-  ),
-  createData(
-    4,
-    "15 Mar, 2019",
-    "Bruce Springsteen",
-    "Long Branch, NJ",
-    "VISA ⠀•••• 5919",
-    212.79
-  )
-];
+import { rows } from "./OrderData";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -66,6 +19,14 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const colHeaders = [
+  { title: "Date" },
+  { title: "Name" },
+  { title: "Ship To" },
+  { title: "Payment Method" },
+  { title: "Sale Amount", align: "right" }
+];
+
 const Orders = () => {
   const classes = useStyles();
   return (
@@ -74,11 +35,11 @@ const Orders = () => {
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            {colHeaders.map(header => (
+              <TableCell key={header.title} align={header.align}>
+                {header.title}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
