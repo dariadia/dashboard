730a296cebd4d212c60b08736c26abe08df7c01e
Daria Diachkova <dariadiachkova@mail.ru>
2020-01-16 16:04:36 +0300

    default set. to be altered

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+//import { ThemeProvider } from "@material-ui/core/styles";
+
+const App = () => {
+  // const theme = {
+  //   background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)"
+  // };
+
+  return (
+    //<ThemeProvider theme={theme}>
+    <Dashboard />
+    //  </ThemeProvider>
+  );
+};
+
+export default App;
